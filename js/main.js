3097168ae8017081cba6b73c5a67d3cbb13aa16c
Marisol Rodriguez <wvk_28@hotmail.com>
2023-06-01 14:08:15 -0300

    Maquetado de landing page

---
+++ js/main.js
@@ -0,0 +1,35 @@
+window.sr = ScrollReveal();
+    
+    sr.reveal('.img', {
+        duration: 5000,
+        origin: 'right',
+        distance: '-100px'
+    });
+    sr.reveal('.img2', {
+        duration: 5000,
+        origin: 'left',
+        distance: '-100px'
+    });
+    sr.reveal('#footer', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    });
+    sr.reveal('#footer-content', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    });
+    sr.reveal('.waves2', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+
+
+
+
+
+
+
